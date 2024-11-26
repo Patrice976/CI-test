3c9ed1c9a850a8e64edbe4d3248db577ec2b35f1
Patrice976 <catinatp@gmail.com>
2024-11-26 15:26:31 +0100

    test linter

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,10 +1,10 @@
-export const fizzbuzz = (number) => {
-    let data = ''
-    if (number % 3 == 0) {
-        data += "Fizz"
-    }
-    if (number % 5 == 0) {
-        data += "Buzz"
-    }
-    return data || number
-}
+export let fizzbuzz = function (number) {
+  let data = "";
+  if (number % 3 == 0) {
+    data += "Fizz";
+  }
+  if (number % 5 == 0) {
+    data += "Buzz";
+  }
+  return data || number;
+};
